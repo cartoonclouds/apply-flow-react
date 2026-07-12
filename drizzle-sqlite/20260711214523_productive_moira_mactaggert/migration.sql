8938155ad048f0509b979eba3c23b6eb0dff40ae
Chris Tudhope <ctudhope@adam.com.au>
2026-07-12 22:40:31 +0100

    feat: remove obsolete migration and snapshot files for applications and contacts

--- drizzle-sqlite/20260711214523_productive_moira_mactaggert/migration.sql
+++
@@ -1,3 +0,0 @@
-CREATE VIEW IF NOT EXISTS "applications_contacts" AS SELECT * FROM "application_contacts";
---> statement-breakpoint
-CREATE VIEW IF NOT EXISTS "applications_documents" AS SELECT * FROM "application_documents";
